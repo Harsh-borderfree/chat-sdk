e8c82f1658b6ad1ce6e1ae58a4b8b3b02944b365
harsh.singh@borderfree.io-at-505103238211 <harsh.singh@borderfree.io>
2022-12-22 16:48:29 +0530

    tryOn message implemented

--- src/ChatMessageBox/ChatImageMessage.js
+++ src/ChatMessageBox/ChatImageMessage.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
-const ChatImageMessage = () => {
-  return <div>ChatImageMessage</div>
+const ChatImageMessage = props => {
+  const { t } = useTranslation()
+  const { messageData } = props
+  return (
+    <>
+      <img className='message-box-photo' src={messageData?.message_text}></img>
+      <p className='MuiTypography-subtitle2 text-message-content'>{`${t('admintab.tryon_text')}`}</p>
+    </>
+  )
 }
 
 export default ChatImageMessage
